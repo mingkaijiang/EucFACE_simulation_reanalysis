a63766da70ff90bf94dc4ecb65cff339c2e4c937
mingkaijiang <shanyujordan512@gmail.com>
2019-09-16 14:03:34 +1000

    cue added

--- scripts/make_parameter_summary_table.R
+++ scripts/make_parameter_summary_table.R
@@ -1,4 +1,13 @@
 make_parameter_summary_table <- function() {
+    
+    ### calculate CUE
+    cue.aco2.old <- with(pChain.aCO2, (mean(NPP.leaf) + mean(NPP.wood) + mean(NPP.froot)) / mean(GPP))
+    cue.aco2.new <- mean(pChain.aCO2$CUE)
+    
+    cue.eco2.old <- with(pChain.eCO2, (mean(NPP.leaf) + mean(NPP.wood) + mean(NPP.froot)) / mean(GPP))
+    cue.eco2.new <- mean(pChain.eCO2$CUE)
+    
+    
     r1 <- predict_final_output_2(pChain = pChain.aCO2, 
                                  obs = obsDF[4,],
                                  return.option = "Return final parameter")
@@ -10,6 +19,15 @@ make_parameter_summary_table <- function() {
     
     out <- rbind(r1, r2)
     out$Trt <- c("aCO2", "eCO2")
+    
+    out[out$Trt=="aCO2", "CUE_old"] <- cue.aco2.old
+    out[out$Trt=="aCO2", "CUE_new"] <- cue.aco2.new
+    
+    out[out$Trt=="eCO2", "CUE_old"] <- cue.eco2.old
+    out[out$Trt=="eCO2", "CUE_new"] <- cue.eco2.new
+    
+    
+    
     
     write.csv(out, "output/parameter_summary_table.csv", row.names=F)
     
